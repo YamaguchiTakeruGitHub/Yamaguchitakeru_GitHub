41f01fc9ad5410db6c1d1a23eefa854841131782
Yamaguchitakeru <mmikan437@gmail.com>
2024-09-03 16:08:46 +0900

    2024.9.3

--- TakeruHearts/TakeruHearts/TakeruHearts/GreenGround.h
+++ TakeruHearts/TakeruHearts/TakeruHearts/GreenGround.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <DxLib.h>
 class GreenGround
 {
 public:
-
+	GreenGround();
+	~GreenGround();
+	
+	void Init();
+	void Update();
+	void Draw();
+	void Final();
+	
+private:
+	int modelHandle;
+	VECTOR position;
 };
 
