2dabccc5fedacb2ffdc3f99db3eb25d2785d2f6a
Yamaguchitakeru <mmikan437@gmail.com>
2024-09-03 16:22:15 +0900

    2024.9.3

--- TakeruHearts/TakeruHearts/TakeruHearts/GreenGround.cpp
+++ TakeruHearts/TakeruHearts/TakeruHearts/GreenGround.cpp
@@ -1,28 +1,28 @@
-#include "GreenGround.h"
-
-GreenGround::GreenGround()
-	: modelHandle()
-	, position()
-{
-}
-
-GreenGround::~GreenGround()
-{
-	MV1DeleteModel();
-}
-
-void GreenGround::Init()
-{
-}
-
-void GreenGround::Update()
-{
-}
-
-void GreenGround::Draw()
-{
-}
-
-void GreenGround::Final()
-{
-}
+//#include "GreenGround.h"
+//
+//GreenGround::GreenGround()
+//	: modelHandle()
+//	, position()
+//{
+//}
+//
+//GreenGround::~GreenGround()
+//{
+//	MV1DeleteModel();
+//}
+//
+//void GreenGround::Init()
+//{
+//}
+//
+//void GreenGround::Update()
+//{
+//}
+//
+//void GreenGround::Draw()
+//{
+//}
+//
+//void GreenGround::Final()
+//{
+//}
